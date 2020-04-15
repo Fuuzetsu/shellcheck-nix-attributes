2db67c428f40e2dcfd6dde6770e68cbb4c05a6e2
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2020-04-15 14:21:25 +0900

    Change API to be a bit more direct, update comments a bit.

--- default.nix
+++ default.nix
@@ -16,37 +16,37 @@ let
     ++ [ pathStr ]
   );
 in
-{ # Takes a collection of attributes and a derivation. Returns a
-  # derivation that has one extra phase at the beginning: one that
-  # checks scripts of every attribute that was asked for.
-  #
-  # This allows us to have a runtime check for late phases (such as
-  # installPhase) but at the very start of the build.
-  shellcheckAttributes = attrNames:
-    if attrNames == []
-    then lib.id
-    else drv: drv.overrideAttrs (attrs:
-      let
-        # Instead of having only one phase, have one phase per thing
-        # being checked. This makes it more obvious what's running in
-        # the CLI output.
-        shellcheckPhases =
-          let attrStrings = lib.getAttrs attrNames attrs;
-          in lib.mapAttrs' (attrName: attrStr:
-            let n = "shellcheck_${attrName}";
-            in lib.nameValuePair n (check (writeText "${drv.name}_${n}" attrStr))
-          ) attrStrings;
-        addPhasesTo = attrName:
-          if lib.hasAttr attrName attrs
-          then builtins.attrNames shellcheckPhases ++ attrs.${attrName}
-          else builtins.attrNames shellcheckPhases;
-        # We have to figure out the least intrusive way to run our
-        # phase. Notably, we don't want to set phases if they weren't
-        # set to start with.
-        injectPhases =
-          if attrs ? phases
-          then { phases = addPhasesTo "phases"; }
-          else { prePhases = addPhasesTo "prePhases"; };
-      in shellcheckPhases // injectPhases
-    );
-}
+# Takes a derivation and checks its phases.
+#
+# If shellcheckAttributes is set, that's what gets checked. If
+# shellcheckAttributes is not set, whatever is set in phases attribute
+# is checked.
+#
+# If neither is set, doesn't check anything.
+drv: drv.overrideAttrs (attrs:
+  let
+    # First check if shellcheckAttributes was set, then check phases.
+    attrNames = attrs.shellcheckAttributes or attrs.phases or [];
+
+    # Instead of having only one phase, have one phase per thing being
+    # checked. This makes it more obvious what's running in the CLI
+    # output.
+    shellcheckPhases =
+      let attrStrings = lib.getAttrs attrNames attrs;
+      in lib.mapAttrs' (attrName: attrStr:
+        let n = "shellcheck_${attrName}";
+        in lib.nameValuePair n (check (writeText "${drv.name}_${n}" attrStr))
+      ) attrStrings;
+
+    # We have to figure out the least intrusive way to run our
+    # phase. Notably, we don't want to set phases if they weren't
+    # set to start with.
+    #
+    # If phases is set, that's where new phases are added, otherwise
+    # we add to prePhases which runs by default.
+    injectPhases = newPhases: if attrs ? phases
+      then { phases = newPhases ++ attrs.phases; }
+      else { prePhases = newPhases ++ attrs.prePhases or []; };
+
+  in shellcheckPhases // injectPhases (builtins.attrNames shellcheckPhases)
+)
